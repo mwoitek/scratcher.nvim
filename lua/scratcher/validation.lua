5478fb0139d37ef0ee5e2a85e5dfe990a611996b
Marcio Woitek <mwoitekjr@gmail.com>
2025-07-12 12:33:51 -0300

    simplify some stuff

--- lua/scratcher/validation.lua
+++ lua/scratcher/validation.lua
@@ -1,33 +1,13 @@
-local is_integer = require("scratcher.utils").is_integer
-
 local M = {}
 
 ---@param x any
 ---@return boolean
-function M.is_dict(x) return type(x) == "table" and (vim.tbl_isempty(x) or not vim.islist(x)) end
-
----@param x any
----@return boolean
-function M.is_non_negative(x) return type(x) == "number" and x >= 0 end
+function M.is_positive(x) return type(x) == "number" and x > 0 end
 
 ---@param x any
 ---@return boolean
 function M.is_valid_position(x)
-  return type(x) == "string" and vim.tbl_contains({ "bottom", "top", "left", "right" }, x)
-end
-
----@param x any
----@return boolean
-function M.is_valid_width(x)
-  if type(x) ~= "number" then return false end
-  return x > 0 and x < (is_integer(x) and vim.o.columns or 1)
-end
-
----@param x any
----@return boolean
-function M.is_valid_height(x)
-  if type(x) ~= "number" then return false end
-  return x > 0 and x < (is_integer(x) and vim.o.lines or 1)
+  return type(x) == "string" and vim.tbl_contains({ "above", "below", "left", "right", "float" }, x)
 end
 
 return M
--- lua/scratcher/win.lua
+++ lua/scratcher/win.lua
@@ -1,23 +1,34 @@
 local M = {}
 
-local floor = math.floor
-local min = math.min
-local validate = vim.validate
+local valid = require "scratcher.validation"
 
 ---@param width number
 ---@return integer
 function M.compute_width(width)
-  validate("width", width, function(x) return type(x) == "number" and x > 0 end, "positive number")
-  local factor = floor(width) == width and 1 or vim.o.columns
-  return min(floor(width * factor), vim.o.columns - 1)
+  vim.validate("width", width, valid.is_positive, "positive number")
+  local factor = math.floor(width) == width and 1 or vim.o.columns
+  return math.min(math.floor(width * factor), vim.o.columns - 1)
 end
 
 ---@param height number
 ---@return integer
 function M.compute_height(height)
-  validate("height", height, function(x) return type(x) == "number" and x > 0 end, "positive number")
-  local factor = floor(height) == height and 1 or vim.o.lines
-  return min(floor(height * factor), vim.o.lines - 1)
+  vim.validate("height", height, valid.is_positive, "positive number")
+  local factor = math.floor(height) == height and 1 or vim.o.lines
+  return math.min(math.floor(height * factor), vim.o.lines - 1)
+end
+
+---@param position scratcher.Position
+---@param size scratcher.Size
+---@return vim.api.keyset.win_config
+function M.get_win_config(position, size)
+  vim.validate("position", position, valid.is_valid_position, "valid window position")
+
+  local config = {}
+
+  -- TODO
+
+  return config
 end
 
 return M
