291bbbd657225f99de545bab019e3c98ad83adb2
Marcio Woitek <mwoitekjr@gmail.com>
2025-07-12 16:21:17 -0300

    improve code for opening window

--- lua/scratcher/validation.lua
+++ lua/scratcher/validation.lua
@@ -6,6 +6,10 @@ function M.is_positive(x) return type(x) == "number" and x > 0 end
 
 ---@param x any
 ---@return boolean
+function M.is_positive_integer(x) return M.is_positive(x) and math.floor(x) == x end
+
+---@param x any
+---@return boolean
 function M.is_valid_position(x)
   return type(x) == "string" and vim.tbl_contains({ "above", "below", "left", "right", "float" }, x)
 end
--- lua/scratcher/win.lua
+++ lua/scratcher/win.lua
@@ -18,15 +18,41 @@ function M.compute_height(height)
   return math.min(math.floor(height * factor), vim.o.lines - 1)
 end
 
+---@param rows integer
+---@param cols integer
+---@return integer row
+---@return integer col
+function M.compute_float_coordinates(rows, cols)
+  vim.validate("rows", rows, valid.is_positive_integer, "positive integer")
+  vim.validate("cols", cols, valid.is_positive_integer, "positive integer")
+  local row = math.floor((vim.o.lines - rows) / 2)
+  local col = math.floor((vim.o.columns - cols) / 2)
+  return row, col
+end
+
 ---@param position scratcher.Position
 ---@param size scratcher.Size
 ---@return vim.api.keyset.win_config
-function M.get_win_config(position, size)
+function M.get_window_geometry(position, size)
   vim.validate("position", position, valid.is_valid_position, "valid window position")
+  -- NOTE: size is validated by the functions that use its value
 
   local config = {}
 
-  -- TODO
+  if position == "float" then
+    config.relative = "editor"
+    config.width = M.compute_width(size.width)
+    config.height = M.compute_height(size.height)
+    config.row, config.col = M.compute_float_coordinates(config.height, config.width)
+  else
+    config.split = position
+    config.win = -1
+    if position == "left" or position == "right" then
+      config.width = M.compute_width(size.width)
+    else
+      config.height = M.compute_height(size.height)
+    end
+  end
 
   return config
 end
