3b0f274ab322ffa72f6233203561f07c48d2ac30
Marcio Woitek <mwoitekjr@gmail.com>
2025-07-15 19:06:54 -0300

    update options class

--- lua/scratcher/options.lua
+++ lua/scratcher/options.lua
@@ -1,82 +1,25 @@
-local valid = require "scratcher.validation"
+---@alias scratcher.Position "above"|"below"|"left"|"right"|"float"
+---@alias scratcher.Size { width: number, height: number }
 
----@alias scratcher.Position "bottom"|"left"|"right"|"top"|"float"
----@alias scratcher.SizeOpts { width: number, height: number }
-
----@alias auto_hide { enable: boolean, timeout: number }
-
----@class Options
+---@class scratcher.Opts
 ---@field position scratcher.Position
----@field size scratcher.SizeOpts
+---@field size scratcher.Size
 ---@field start_in_insert boolean
----@field auto_hide auto_hide
-local Options = {
-  position = "bottom",
+
+---@type scratcher.Opts
+local Opts = {
+  position = "below",
   size = { width = 0.5, height = 0.33 },
   start_in_insert = false,
-  auto_hide = {
-    enable = false,
-    timeout = 10, -- minutes
-  },
 }
+Opts.__index = Opts
 
----@param raw_opts any
----@return Options
-function Options:new(raw_opts)
-  local opts = {}
-  setmetatable(opts, { __index = self })
-
-  if not valid.is_dict(raw_opts) then return opts end
-
-  for _, opt in ipairs { "position", "width", "height" } do
-    opts[opt] = valid["is_valid_" .. opt](raw_opts[opt]) and raw_opts[opt] or nil
-  end
-
-  if type(raw_opts.start_in_insert) == "boolean" then opts.start_in_insert = raw_opts.start_in_insert end
-
-  local raw_ah = raw_opts.auto_hide
-  if valid.is_dict(raw_ah) then
-    local ah = {}
-    setmetatable(ah, { __index = self.auto_hide })
-
-    if type(raw_ah.enable) == "boolean" then ah.enable = raw_ah.enable end
-    if valid.is_non_negative(raw_ah.timeout) then ah.timeout = raw_ah.timeout end
-
-    opts.auto_hide = not vim.tbl_isempty(ah) and ah or nil
-  end
-
-  return opts
+---@param opts table?
+---@return scratcher.Opts
+function Opts.new(opts)
+  vim.validate("opts", opts, "table", true, "options table or nil")
+  opts = vim.tbl_deep_extend("force", Opts, opts or {})
+  return setmetatable(opts, Opts)
 end
 
----@alias dimension "width" | "height"
-
----@param dim dimension
----@return number
-function Options:get_dimension(dim)
-  vim.validate {
-    dim = {
-      dim,
-      function(d) return d == "width" or d == "height" end,
-      "width or height",
-    },
-  }
-
-  local val = self[dim]
-  ---@cast val number
-
-  if require("scratcher.utils").is_integer(val) then return val end
-
-  local max_val = dim == "width" and vim.o.columns or vim.o.lines
-  return math.floor(val * max_val)
-end
-
----@return string
-function Options:split_cmd()
-  local pos = self.position
-  if pos == "bottom" or pos == "top" then
-    return string.format("%s %d split", pos == "bottom" and "bel" or "abo", self:get_dimension "height")
-  end
-  return string.format("%s %d vsplit", pos == "right" and "bel" or "abo", self:get_dimension "width")
-end
-
-return Options
+return Opts
