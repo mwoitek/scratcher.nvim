a29ed1fae7824ec2c4c9316950a00ea64f81dad1
Marcio Woitek <mwoitekjr@gmail.com>
2026-06-03 01:56:19 -0300

    complete Document class

--- lua/scratcher/document.lua
+++ lua/scratcher/document.lua
@@ -3,6 +3,7 @@ Document.__index = Document
 
 local api = vim.api
 local fn = vim.fn
+local uv = vim.uv
 
 function Document:new(name, path)
   local o = { name = name, path = path, buf = nil }
@@ -18,25 +19,54 @@ function Document:buffer()
   return self.buf
 end
 
-function Document:load_buffer()
-  if self:is_loaded() then return self.buf end
-  self.buf = fn.bufadd(self.path)
-  fn.bufload(self.buf)
-  vim.bo[self.buf].bufhidden = "hide"
-  vim.bo[self.buf].buflisted = false
-  return self.buf
+function Document:exists()
+  local stat = uv.fs_stat(self.path)
+  return type(stat) == "table" and stat.type == "file"
 end
 
-function Document:unload_buffer()
+function Document:create()
+  if self:exists() then return end
+
+  local mode = tonumber("644", 8)
+  local file = uv.fs_open(self.path, "w", mode)
+  if type(file) ~= "number" then
+    local err = string.format("Failed to open file: %s", self.path)
+    error(err)
+  end
+
+  local bytes_written = uv.fs_write(file, "")
+  if type(bytes_written) ~= "number" then
+    local err = string.format("Failed to write to file: %s", self.path)
+    error(err)
+  end
+
+  local closed = uv.fs_close(file)
+  if type(closed) ~= "boolean" or not closed then
+    local err = string.format("Failed to close file: %s", self.path)
+    error(err)
+  end
+end
+
+function Document:load()
+  if self:is_loaded() then return end
+
+  local ok, err = pcall(self.create, self)
+  if not ok then error(err) end
+
+  self.buf = fn.bufadd(self.path)
+  fn.bufload(self.buf)
+
+  vim.bo[self.buf].bufhidden = "hide"
+  vim.bo[self.buf].buflisted = false
+end
+
+function Document:unload()
   api.nvim_buf_delete(self:buffer(), { unload = true })
   self.buf = nil
 end
 
--- TODO: add "create" method
-
-function Document:exists()
-  local stat = vim.uv.fs_stat(self.path)
-  return type(stat) == "table" and stat.type == "file"
+function Document:clear()
+  api.nvim_buf_set_lines(self:buffer(), 0, -1, true, {})
 end
 
 function Document:save()
