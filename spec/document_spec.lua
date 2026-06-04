452f69cd922915cfd67073b9f8cc8133adc13c59
Marcio Woitek <mwoitekjr@gmail.com>
2026-06-04 06:58:24 -0300

    add more tests for the Document class

--- spec/document_spec.lua
+++ spec/document_spec.lua
@@ -1,9 +1,17 @@
 local assert = require("luassert")
 
+local api = vim.api
 local fn = vim.fn
 
 describe("Document", function()
   local Document = require("scratcher.document")
+
+  teardown(function()
+    local bufs = api.nvim_list_bufs()
+    for _, buf in ipairs(bufs) do
+      if api.nvim_buf_is_loaded(buf) then pcall(api.nvim_buf_delete, buf, { unload = true }) end
+    end
+  end)
 
   describe(":is_loaded()", function()
     it("returns false for an unloaded buffer", function()
@@ -15,6 +23,17 @@ describe("Document", function()
     end)
   end)
 
+  describe(":create()", function()
+    it("creates the file if path refers to a non-existing file", function()
+      local name = "non_existing_doc"
+      local path = fn.tempname()
+      local doc = Document:new(name, path)
+      assert.is_false(doc:exists())
+      doc:create()
+      assert.is_true(doc:exists())
+    end)
+  end)
+
   describe(":load()", function()
     it("loads an existing file", function()
       local name = "existing_doc"
@@ -23,6 +42,18 @@ describe("Document", function()
       doc:load()
       local loaded = doc:is_loaded()
       assert.is_true(loaded)
+      local buf = doc:buffer()
+      assert.is_true(type(buf) == "number")
+    end)
+
+    it("loads a file that has to be created first", function()
+      local name = "non_existing_doc"
+      local path = fn.tempname()
+      local doc = Document:new(name, path)
+      assert.is_false(doc:exists())
+      doc:load()
+      assert.is_true(doc:exists())
+      assert.is_true(doc:is_loaded())
       local buf = doc:buffer()
       assert.is_true(type(buf) == "number")
     end)
